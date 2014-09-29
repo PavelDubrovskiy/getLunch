fc61ee11cd5ae8a57edb6e8dc6c4337d3f98337c
PavelDubrovskiy <freecrus@gmail.com>
2014-09-29 11:27:12 +0400

    Revert "Second Commit"
    This reverts commit 3002844a4f11b179e6fce6e93beb6fc169dab5a1.

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -34,73 +34,6 @@ module.exports = [
         "clobbers": [
             "navigator.accelerometer"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/contacts.js",
-        "id": "org.apache.cordova.contacts.contacts",
-        "clobbers": [
-            "navigator.contacts"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/Contact.js",
-        "id": "org.apache.cordova.contacts.Contact",
-        "clobbers": [
-            "Contact"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/ContactAddress.js",
-        "id": "org.apache.cordova.contacts.ContactAddress",
-        "clobbers": [
-            "ContactAddress"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/ContactError.js",
-        "id": "org.apache.cordova.contacts.ContactError",
-        "clobbers": [
-            "ContactError"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/ContactField.js",
-        "id": "org.apache.cordova.contacts.ContactField",
-        "clobbers": [
-            "ContactField"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/ContactFindOptions.js",
-        "id": "org.apache.cordova.contacts.ContactFindOptions",
-        "clobbers": [
-            "ContactFindOptions"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/ContactName.js",
-        "id": "org.apache.cordova.contacts.ContactName",
-        "clobbers": [
-            "ContactName"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/ContactOrganization.js",
-        "id": "org.apache.cordova.contacts.ContactOrganization",
-        "clobbers": [
-            "ContactOrganization"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.contacts/www/ContactFieldType.js",
-        "id": "org.apache.cordova.contacts.ContactFieldType"
-    },
-    {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -109,9 +42,7 @@ module.exports.metadata =
     "org.apache.cordova.device-orientation": "0.3.7",
     "org.apache.cordova.geolocation": "0.3.8",
     "org.apache.cordova.device-motion": "0.2.8",
-    "org.apache.cordova.console": "0.2.9",
-    "org.apache.cordova.contacts": "0.2.12",
-    "org.apache.cordova.inappbrowser": "0.5.1"
+    "org.apache.cordova.console": "0.2.9"
 }
 // BOTTOM OF METADATA
 });
--- www/js/application.js
+++ www/js/application.js
@@ -1,21 +1,47 @@
-var tmp = new Array();		// два вспомагательных
-var tmp2 = new Array();		// массива
-var param = new Array();
+// Compass by Pavel
+//var watchID= navigator.compass.watchHeading(onSuccessHeading, onErrorHeading,options);
+function onSuccessHeading(heading) {
+    //alert('Heading: ' + heading.magneticHeading);
+    var now=Math.abs(360-heading.magneticHeading);
+   	$('#compasRow').css({'-webkit-transform':'rotate('+now+'deg)','-moz-transform':'rotate('+now+'deg)','-ms-transform':'rotate('+now+'deg)','-o-transform':'rotate('+now+'deg)','transform':'rotate('+now+'deg)'});
+    $('#heading').text(heading.magneticHeading);
+};
+function onErrorHeading(error) {
+    alert('CompassError: ' + error.code);
+};
+var options = {
+    frequency: 500
+};
 
-var get = location.search;	// строка GET запроса
-if(get != '') {
-	tmp = (get.substr(1)).split('&');	// разделяем переменные
-	for(var i=0; i < tmp.length; i++) {
-		tmp2 = tmp[i].split('=');		// массив param будет содержать
-		param[tmp2[0]] = tmp2[1];		// пары ключ(имя переменной)->значение
-	}
+
+
+// onSuccess Callback
+// This method accepts a Position object, which contains the
+// current GPS coordinates
+//
+var onSuccessPosition = function(position) {
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+};
+
+// onError Callback receives a PositionError object
+//
+function onErrorPosition(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
 }
-jQuery.fn.rotate=function(degrees){
-	$(this).css({
-	  '-webkit-transform' : 'rotate('+degrees+'deg)',
-	     '-moz-transform' : 'rotate('+degrees+'deg)',  
-	      '-ms-transform' : 'rotate('+degrees+'deg)',  
-	       '-o-transform' : 'rotate('+degrees+'deg)',  
-	          'transform' : 'rotate('+degrees+'deg)',
-    });
+function tryAll(){
+	navigator.compass.getCurrentHeading(onSuccessHeading, onErrorHeading);
+	//navigator.geolocation.getCurrentPosition(onSuccessPosition, onErrorPosition);
 }
+
+$(document).ready(function(){
+	//alert('ddddd');
+});
+setInterval(tryAll, 100);
