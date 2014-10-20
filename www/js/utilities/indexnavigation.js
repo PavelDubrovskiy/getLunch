c6b59de7765ef0c5cf4736d19dc3e8fc24ef5448
getlunch.ru <getlup@dedi797.your-server.de>
2014-10-20 16:22:11 +0200

    20.10.2014 18.25

--- www/js/utilities/indexnavigation.js
+++ www/js/utilities/indexnavigation.js
@@ -2,30 +2,51 @@
 	var $ = Framework7.$;
 	
 	function IndexNavigation(values) {
-		var panel = $(document.createElement('div'));
 		var alphabet = '';
-		var i = 0;
+		var i;
 		
 		values = values || {};
 		
 		this.container = $(values['container'] || '.b_index-nav');
+		this.scrollContainer = $(values['scrollContainer'] || '.page-content');
 		this.sections = this.container.find(values['sections'] || '.list-group-title');
 		
-		panel.addClass('b_index-nav_panel');
-		this.container.append(panel);
+		this.panel = $(document.createElement('div'));
+		this.panel.addClass('b_index-nav_panel');
+		this.container.append(this.panel);
+		this.panelOffset = this.panel.offset().top - 44;
 		
 		for(i = 0; i < this.sections.length; i++) {
 			$(this.sections[i]).attr('id', 'indexNavSection_' + i);
 			alphabet += '<span class="b_index-nav_link" data-id="indexNavSection_' + i + '">' + $(this.sections[i]).text() + '</span>';
 		}
 		
-		panel.append(alphabet);
+		this.panel.append(alphabet);
 		
-		//this.sections.on('touchmove', this.onTouchMove.bind(this));
+		this.panel.on('click', '.b_index-nav_link', this.touchMove.bind(this));
+		this.scrollContainer.on('scroll', this.fixPanelPosition.bind(this));
 	}
 	
-	IndexNavigation.prototype.onTouchMove = function(e) {
-		console.log(e);
+	IndexNavigation.prototype.touchMove = function(e) {
+		e.stopPropagation();
+		var target = e.target;//document.elementFromPoint(e.touches[0].pageX, e.touches[0].pageY);
+		//if($(target).hasClass('b_index-nav_link')) {
+			//this.scrollContainer.scrollTop(, 400);
+		//}
+		
+		console.log(this.scrollContainer.scrollTop());
+		
+		this.scrollContainer.scrollTop($('#'+$(target).data('id')).offset().top - this.scrollContainer[0].scrollTop);
+	};
+	
+	IndexNavigation.prototype.fixPanelPosition = function(e) {
+		var scroll = e.target.scrollTop;
+		e.stopPropagation();		
+		if(scroll > this.panelOffset) {
+			this.panel.transform('translate3d(0,'+ (scroll - this.panelOffset) +'px,0)');
+		}else{
+			this.panel.transform('translate3d(0,0,0)');
+		}
 	};
 	
 	return IndexNavigation;
