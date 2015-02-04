89c524e2203385efdc47811b7d5f8bdfaaf22b66
getlunch.ru <getlup@dedi797.your-server.de>
2015-02-04 15:33:54 +0100

    Updated 04.02.2015 17.38

--- www/js/vc/about/aboutController.js
+++ www/js/vc/about/aboutController.js
@@ -1,9 +1,25 @@
-define(["app"], function(app) {	
-	// Инициализация страницы
+define(["app","js/vc/start/startView"], function(app,view) {	
+	var bindings = [
+		{
+			element: '#externalSiteOneTouch',
+			event: 'click',
+			handler: externalSiteLoadOneTouch
+		}
+	];
 	function init(query) {
+		view.render({
+			bindings: bindings
+		});
 	}
 	
 	return {
 		init: init
 	};
+	function externalSiteLoadOneTouch(){
+		if( navigator.app ){
+		    navigator.app.loadUrl('http://one-touch.ru', {openExternal:true});
+		}else{
+		    window.open('http://one-touch.ru', "_system" );
+		}
+	}
 });
